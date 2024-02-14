c5a02f83f8220e643bd4a423a847488abfd15ba9
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2024-02-14 16:47:29 -0700

    add globby override

--- server/core/entry.dev.ts
+++ server/core/entry.dev.ts
@@ -6,47 +6,9 @@ import { Server, setServer } from './server'
 // @ts-expect-error it is there
 import { trapUnhandledNodeErrors } from '#internal/nitro/utils'
 
-// @ts-expect-error it is there
-import { runNitroTask } from '#internal/nitro/task'
-
-// @ts-expect-error it is there
-import { tasks } from '#internal/nitro/virtual/tasks'
-
-// console.log('custom dev server')
 const nitroApp = useNitroApp()
 
-// @ts-expect-error H3App is App
 const handler = toWebHandler(nitroApp.h3App)
-
-nitroApp.router.get(
-  '/_nitro/tasks',
-  // @ts-expect-error type import errors
-  defineEventHandler((event) => {
-    return {
-      tasks: Object.fromEntries(
-        Object.entries(tasks).map(([name, task]) => [
-          name,
-          // @ts-expect-error type import errors
-          { description: task.description },
-        ]),
-      ),
-    }
-  },
-  ),
-)
-nitroApp.router.use(
-  '/_nitro/tasks/:name',
-  // @ts-expect-error type import errors
-  defineEventHandler(async (event) => {
-    const name = getRouterParam(event, 'name')
-    const payload = {
-      ...getQuery(event),
-      ...(await readBody(event).catch(() => ({}))),
-    }
-    return await runNitroTask(name, payload)
-  },
-  ),
-)
 
 // const routes = nitroApp.h3App.stack
 // console.log('routes', routes);
@@ -64,7 +26,6 @@ const server = Bun.serve({
   websocket,
 })
 
-// @ts-expect-error event is typed differently
 nitroApp.router.get('/api/server', defineEventHandler(event => server.port))
 // console.log('server', server);
 
